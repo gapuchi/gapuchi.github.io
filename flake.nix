bd609c22c35bf0a6c5197f563077a22c62613674
gapuchi <arjun.adhia@gmail.com>
2025-10-22 13:53:58 -0400

    Add Nix Run Command

--- flake.nix
+++ flake.nix
@@ -8,16 +8,29 @@
   outputs = { self, nixpkgs }: 
     let
       eachDefaultSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ];
+      mkBuildInputs = system: with nixpkgs.legacyPackages.${system}; [
+          (python3.withPackages (python-pkgs: [
+            python-pkgs.mkdocs
+            python-pkgs.mkdocs-material
+          ]))
+        ];
     in
     {
       devShells = eachDefaultSystem (system: {
         default = nixpkgs.legacyPackages.${system}.mkShell {
-          buildInputs = with nixpkgs.legacyPackages.${system}; [
-            (python3.withPackages (python-pkgs: [
-              python-pkgs.mkdocs
-              python-pkgs.mkdocs-material
-            ]))
-          ];
+          buildInputs = mkBuildInputs system;
+        };
+      });
+
+      apps = eachDefaultSystem (system: {
+        serve = {
+          type = "app";
+          program = "${nixpkgs.legacyPackages.${system}.runCommand "serve" {
+            buildInputs = mkBuildInputs system;
+          } ''
+            cd ${self}
+            mkdocs serve
+          ''}";
         };
       });
     };
