613ee15dfefd75ed739b98bcafe461e1ad67b271
gapuchi <arjun.adhia@gmail.com>
2025-10-22 20:30:34 -0400

    Remove Dev Shell

--- flake.nix
+++ flake.nix
@@ -3,35 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: 
-    let
-      eachDefaultSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ];
-      mkBuildInputs = system: with nixpkgs.legacyPackages.${system}; [
-          (python3.withPackages (python-pkgs: [
-            python-pkgs.mkdocs
-            python-pkgs.mkdocs-material
-          ]))
-        ];
-    in
-    {
-      devShells = eachDefaultSystem (system: {
-        default = nixpkgs.legacyPackages.${system}.mkShell {
-          buildInputs = mkBuildInputs system;
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mkdocs = pkgs.python3.withPackages (python-pkgs: [
+          python-pkgs.mkdocs
+          python-pkgs.mkdocs-material
+        ]);
+      in
+      {
+        apps = {
+          default = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "serve" ''
+              cd ${self}
+              ${mkdocs}/bin/mkdocs serve
+            ''}/bin/serve";
+          };
         };
-      });
-
-      apps = eachDefaultSystem (system: {
-        serve = {
-          type = "app";
-          program = "${nixpkgs.legacyPackages.${system}.runCommand "serve" {
-            buildInputs = mkBuildInputs system;
-          } ''
-            cd ${self}
-            mkdocs serve
-          ''}";
-        };
-      });
-    };
+      }
+    );
 }
